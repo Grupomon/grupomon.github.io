1674e04b8e5c3727d022f5ca2fd8dab26dc4437b
Grupomon <grupoprocessamentovideo@gmail.com>
2024-10-24 20:53:27 -0300

    Add files via upload

---
+++ Relatorio_Lab03/Filtro gaussiano aplicado/CMakeFiles/Gaussian_Threshold_inRange.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gaussian_Threshold_inRange.dir/Gaussian_Threshold_inRange.cpp.o"
+  "CMakeFiles/Gaussian_Threshold_inRange.dir/Gaussian_Threshold_inRange.cpp.o.d"
+  "Gaussian_Threshold_inRange"
+  "Gaussian_Threshold_inRange.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gaussian_Threshold_inRange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Relatorio_Lab03/Filtro gaussiano aplicado/CMakeFiles/Gaussian_Threshold_inRange.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Gaussian_Threshold_inRange.
+# This may be replaced when dependencies are built.
