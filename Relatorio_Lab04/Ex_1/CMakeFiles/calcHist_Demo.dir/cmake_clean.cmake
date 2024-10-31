e7720b1f7c9c07c854715f401e003176006e34d6
Grupomon <grupoprocessamentovideo@gmail.com>
2024-10-31 20:40:10 -0300

    Add files via upload

---
+++ Relatorio_Lab04/Ex_1/CMakeFiles/calcHist_Demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calcHist_Demo.dir/calcHist_Demo.cpp.o"
+  "CMakeFiles/calcHist_Demo.dir/calcHist_Demo.cpp.o.d"
+  "calcHist_Demo"
+  "calcHist_Demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calcHist_Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
