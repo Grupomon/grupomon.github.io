fbb90ec89dcf338eb89705c39bc4a55cba56152e
Grupomon <grupoprocessamentovideo@gmail.com>
2024-11-12 22:12:38 -0300

    Add files via upload

---
+++ Video Gravado/CMakeFiles/bg_sub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bg_sub.dir/bg_sub.cpp.o"
+  "CMakeFiles/bg_sub.dir/bg_sub.cpp.o.d"
+  "bg_sub"
+  "bg_sub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bg_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
