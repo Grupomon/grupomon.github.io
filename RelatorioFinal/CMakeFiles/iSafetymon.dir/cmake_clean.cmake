6f57883046faa7d92c77ff1daee2467bdedf1085
Grupomon <grupoprocessamentovideo@gmail.com>
2024-12-10 21:48:07 -0300

    Add files via upload

---
+++ RelatorioFinal/CMakeFiles/iSafetymon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iSafetymon.dir/iSafetymon.cpp.o"
+  "CMakeFiles/iSafetymon.dir/iSafetymon.cpp.o.d"
+  "iSafetymon"
+  "iSafetymon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iSafetymon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
